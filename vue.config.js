10d6fe2e600a377f8db07c5a0d15d4dea7e502c3
Camrado <blackandwhite.b17w@gmail.com>
2024-01-26 19:02:22 +0400

    Another try to change the color of the app from green to dark violet

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
+
+  pwa: {
+    name: 'SkipSmart',
+    short_name: 'SkipSmart',
+    theme_color: '#232946',
+    icons: [
+      { src: './img/icons/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
+      { src: './img/icons/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' },
+      { src: './img/icons/android-chrome-maskable-192x192.png', sizes: '192x192', type: 'image/png', purpose: 'maskable' },
+      { src: './img/icons/android-chrome-maskable-512x512.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' }
+    ],
+    start_url: '.',
+    display: 'standalone',
+    background_color: '#000000'
+  }
 });
