cad0f3bffdcfa6f282a936dd7c13d1070a3909e6
Camrado <blackandwhite.b17w@gmail.com>
2024-01-26 19:03:03 +0400

    Another try to change app color

--- vue.config.js
+++ vue.config.js
@@ -6,12 +6,6 @@ module.exports = defineConfig({
     name: 'SkipSmart',
     short_name: 'SkipSmart',
     theme_color: '#232946',
-    icons: [
-      { src: './img/icons/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
-      { src: './img/icons/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' },
-      { src: './img/icons/android-chrome-maskable-192x192.png', sizes: '192x192', type: 'image/png', purpose: 'maskable' },
-      { src: './img/icons/android-chrome-maskable-512x512.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' }
-    ],
     start_url: '.',
     display: 'standalone',
     background_color: '#000000'
